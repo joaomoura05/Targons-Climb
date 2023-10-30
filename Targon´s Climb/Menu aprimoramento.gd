f11b81d0c56c0759fce7660c92bc552982cf2b79
João Pedro de Moura Medeiros <joaomoura70718@gmail.com>
2023-10-30 20:31:46 -0300

    upgrade menu

---
+++ Targon´s Climb/Menu aprimoramento.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_escolha_1_pressed():
+	get_tree().change_scene_to_file("res://Level II.tscn")
+
+
+func _on_escolha_2_pressed():
+	get_tree().change_scene_to_file("res://Level II.tscn")
+
+
+func _on_escolha_3_pressed():
+	get_tree().change_scene_to_file("res://Level II.tscn")
